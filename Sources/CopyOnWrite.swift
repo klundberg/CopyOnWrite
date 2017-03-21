3bee7dd43396f1c28333005a440f9801254feb35
Kevin Lundberg <kevin@klundberg.com>
2017-03-20 23:35:52 -0400

    Removed reference init parameter name, added sanity check to tests

--- Sources/CopyOnWrite.swift
+++ Sources/CopyOnWrite.swift
@@ -14,35 +14,42 @@ private final class Box<Boxed> {
 }
 
 /// Encapsulates behavior surrounding value semantics and copy-on-write behavior
-public struct CopyOnWrite<T: AnyObject> {
+public struct CopyOnWrite<Reference: AnyObject> {
 
-    private var _reference: Box<T>
-    private let makeCopy: (T) -> T
+    private var _reference: Box<Reference>
+    private let makeCopy: (Reference) -> Reference
 
     /// Constructs the copy-on-write wrapper around the given reference and copy function
     ///
     /// - Parameters:
     ///   - reference: The object that is to be given value semantics
     ///   - copier: The function that is responsible for copying the reference if the consumer of this API needs it to be copied. This function should create a new instance of the referenced type; it should not return the original reference given to it.
-    public init(reference: T, copier: @escaping (T) -> T) {
+    public init(_ reference: Reference, copier: @escaping (Reference) -> Reference) {
         self._reference = Box(reference)
         self.makeCopy = copier
     }
 
-    /// Returns the reference meant for reading only.
-    public var reference: T {
+    /// Returns the reference meant for read-only operations.
+    public var reference: Reference {
         return _reference.unbox
     }
 
-    /// Returns the reference meant for mutating. If necessary, the reference is copied using the function or closure provided to the initializer before returning, in order to preserve value semantics.
-    public var mutatingReference: T {
+    /// Returns the reference meant for mutable operations. If necessary, the reference is copied using the `copier` function or closure provided to the initializer before returning, in order to preserve value semantics.
+    public var mutatingReference: Reference {
         mutating get {
             // copy the reference only if necessary
-            if !isKnownUniquelyReferenced(&_reference) {
+            if !isUniquelyReferenced {
                 _reference = Box(makeCopy(_reference.unbox))
             }
 
             return _reference.unbox
+        }
+    }
+
+    /// Helper property to determine whether the reference is uniquely held. Used in tests as a sanity check.
+    internal var isUniquelyReferenced: Bool {
+        mutating get {
+            return isKnownUniquelyReferenced(&_reference)
         }
     }
 }
@@ -57,40 +64,40 @@ public protocol Cloneable: class {
 }
 
 // MARK: - Cloneable
-extension CopyOnWrite where T: Cloneable {
+extension CopyOnWrite where Reference: Cloneable {
 
     /// Constructs the copy-on-write wrapper around the given `Cloneable` reference
     ///
     /// - Parameter reference: A `Cloneable` object which will be copied with `clone()` when it is to be mutated.
-    public init(reference: T) {
-        self.init(reference: reference, copier: { $0.clone() })
+    public init(_ reference: Reference) {
+        self.init(reference, copier: { $0.clone() })
     }
 }
 
 import Foundation
 
 // MARK: - NSCopying
-extension CopyOnWrite where T: NSCopying {
+extension CopyOnWrite where Reference: NSCopying {
 
     /// Constructs the copy-on-write wrapper around the given `NSCopying` reference
     ///
-    /// CAUTION: If `T` implements both `NSCopying` and `NSMutableCopying`, make sure you only pass an instance of a type that returns the same type when calling `copy()`, otherwise an attempt to mutate this reference could cause your program to trap.
+    /// CAUTION: If `Reference` implements both `NSCopying` and `NSMutableCopying`, make sure you only pass an instance of a type that returns the same type when calling `copy()`, otherwise an attempt to mutate this reference could cause your program to trap.
     ///
     /// - Parameter reference: An `NSCopying` object which will be copied with `copy()` when it is to be mutated.
-    public init(copyingReference reference: T) {
-        self.init(reference: reference, copier: { $0.copy() as! T })
+    public init(copyingReference reference: Reference) {
+        self.init(reference, copier: { $0.copy() as! Reference })
     }
 }
 
 // MARK: - NSMutableCopying
-extension CopyOnWrite where T: NSMutableCopying {
+extension CopyOnWrite where Reference: NSMutableCopying {
 
     /// Constructs the copy-on-write wrapper around the given `NSMutableCopying` reference
     ///
-    /// CAUTION: If `T` implements both `NSCopying` and `NSMutableCopying`, make sure you only pass an instance of a type that returns the same type when calling `mutableCopy()`, otherwise an attempt to mutate this reference could cause your program to trap.
+    /// CAUTION: If `Reference` implements both `NSCopying` and `NSMutableCopying`, make sure you only pass an instance of a type that returns the same type when calling `mutableCopy()`, otherwise an attempt to mutate this reference could cause your program to trap.
     ///
     /// - Parameter reference: An `NSMutableCopying` object which will be copied with `mutableCopy()` when it is to be mutated.
-    public init(mutableCopyingReference reference: T) {
-        self.init(reference: reference, copier: { $0.mutableCopy() as! T })
+    public init(mutableCopyingReference reference: Reference) {
+        self.init(reference, copier: { $0.mutableCopy() as! Reference })
     }
 }
--- Tests/CopyOnWriteTests/CopyOnWriteTests.swift
+++ Tests/CopyOnWriteTests/CopyOnWriteTests.swift
@@ -18,44 +18,58 @@ class CopyOnWriteTests: XCTestCase {
         super.setUp()
 
         values = [
-            CopyOnWrite(reference: Container(), copier: { $0.clone() }),
-            CopyOnWrite(reference: Container()),
+            CopyOnWrite(Container(), copier: { $0.clone() }),
+            CopyOnWrite(Container()),
             CopyOnWrite(copyingReference: Container()),
             CopyOnWrite(mutableCopyingReference: Container()),
         ]
     }
 
     func test_StringDoesNotCopy_WhenAccessingImmutableReference_WhileItIsUniquelyReferenced() {
-        for value in values {
-            value.reference.append("foo")
+        for index in 0..<values.count {
+            XCTAssertTrue(values[index].isUniquelyReferenced) // sanity check
 
-            XCTAssertEqual(value.reference, "foo")
+            values[index].reference.append("foo")
+
+            XCTAssertEqual(values[index].reference, "foo")
+            XCTAssertTrue(values[index].isUniquelyReferenced)
         }
     }
 
     func test_StringDoesNotCopy_WhenAccessingImmutableReference_WhileItIsNotUniquelyReferenced() {
-        for value in values {
-            let old = value
-            value.reference.append("foo")
+        for index in 0..<values.count  {
+            let old = values[index]
+
+            XCTAssertFalse(values[index].isUniquelyReferenced) // sanity check, storing in `old` should make the reference held twice
+
+            values[index].reference.append("foo")
 
             XCTAssertEqual(old.reference, "foo")
+            XCTAssertFalse(values[index].isUniquelyReferenced) // immutable reference doesn't copy on edit, so same reference remaines held by both variables
         }
     }
 
     func test_StringDoesNotCopy_WhenAccessingMutableReference_WhileItIsUniquelyReferenced() {
-        for var value in values {
-            value.mutatingReference.append("foo")
+        for index in 0..<values.count  {
+            XCTAssertTrue(values[index].isUniquelyReferenced) // sanity check
 
-            XCTAssertEqual(value.reference, "foo")
+            values[index].mutatingReference.append("foo")
+
+            XCTAssertEqual(values[index].reference, "foo")
+            XCTAssertTrue(values[index].isUniquelyReferenced)
         }
     }
 
     func test_ReferenceDoesCopy_WhenAccessingMutableReference_WhileItIsNotUniquelyReferenced() {
-        for var value in values {
-            let old = value
-            value.mutatingReference.append("foo")
-            
+        for index in 0..<values.count  {
+            let old = values[index]
+
+            XCTAssertFalse(values[index].isUniquelyReferenced) // sanity check, storing in `old` should make the reference held twice
+
+            values[index].mutatingReference.append("foo")
+
             XCTAssertEqual(old.reference, "")
+            XCTAssertTrue(values[index].isUniquelyReferenced)
         }
     }
 }
